abbdd795b5188be1ae1363dc8bb0185b7ec5a0ac
KevinLeca <leca-kevin@sfr.fr>
2018-04-12 10:33:07 +0200

    Add files via upload

---
+++ Java/CircuitRC/src/calculs/CircuitRC.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculs;
+
+import circuitrc.FenCalcul;
+
+/**
+ *
+ * @author kleca
+ */
+public class CircuitRC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FenCalcul fenetre = new FenCalcul();
+        fenetre.setVisible(true);
+        fenetre.setLocationRelativeTo(null);
+    }
+    
+}
---
+++ Java/dietetique/src/dietetique/Dietetique.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dietetique;
+
+import diet.PoidsIdeal;
+
+/**
+ *
+ * @author kleca
+ */
+public class Dietetique {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PoidsIdeal fenIMC = new PoidsIdeal();
+        fenIMC.setVisible(true);
+        fenIMC.setLocationRelativeTo(null);
+    }
+    
+}
